23b5009d128bbdcefb98e6a4cd2204d020fce87d
Camille Dbschr <c.debusscher@gmail.com>
2022-10-25 16:26:22 +0200

    add(ihm): functionnality clear

--- src/main/java/fr/cda/disquesvinyles/VinyleController.java
+++ src/main/java/fr/cda/disquesvinyles/VinyleController.java
@@ -1,14 +1,64 @@
 package fr.cda.disquesvinyles;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 
 public class VinyleController {
 	@FXML
-	private Button Rec;
+	private TextField title;
 
 	@FXML
-	protected void onHelloButtonClick() {
-//		welcomeText.setText("Welcome to JavaFX Application!");
+	private ComboBox category;
+
+	@FXML
+	private DatePicker pickDate;
+
+	@FXML
+	private TextField priceMin;
+
+	@FXML
+	private TextField priceMax;
+
+	@FXML
+	private Button search;
+
+	@FXML
+	private Button clear;
+
+	@FXML
+	private CheckBox discogs;
+
+	@FXML
+	private CheckBox fnac;
+
+	@FXML
+	private CheckBox vinylcorner;
+
+	@FXML
+	private CheckBox leboncoin;
+
+	@FXML
+	private CheckBox mesvinyles;
+
+	@FXML
+	private CheckBox culturefactory;
+
+	@FXML
+	protected void onClickSearch() {
+	}
+
+	@FXML
+	protected void onClickClear() {
+		title.setText("");
+//		category.set;
+		pickDate.setValue(null);
+		priceMin.setText("");
+		priceMax.setText("");
+		discogs.setSelected(false);
+		fnac.setSelected(false);
+		vinylcorner.setSelected(false);
+		leboncoin.setSelected(false);
+		mesvinyles.setSelected(false);
+		culturefactory.setSelected(false);
 	}
 }
